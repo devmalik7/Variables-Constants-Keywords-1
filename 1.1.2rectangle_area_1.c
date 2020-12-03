8ea0c80350c918f0f66421d647932b2917245c52
devmalik7 <72170432+devmalik7@users.noreply.github.com>
2020-12-03 23:24:59 +0530

    update1.1.2rectangle_area_1.c

--- 1.1.2rectangle_area_1.c
+++ 1.1.2rectangle_area_1.c
@@ -1,11 +1,10 @@
 #include<stdio.h>
 int main()
 {
-    int len,bre;
-    printf("Enter the length of rectangle \n");
-    scanf("%d",& len);
-    printf("Enter the breadth of rectangle \n");
-    scanf("%d",& bre);
-    printf("The area of rectangle is %d", len*bre);
-    return 0;
+    int area=0,len,bre;//taking length breadth,area
+    printf("Enter the length and breadth of rectangle \n");
+    scanf("%d",&len,&bre); //tking the input 
+    area=len*bre;// getting the area 
+    printf("The area of rectangle is %d",area);//printing the area
+    return 0;//returning the main
 }
